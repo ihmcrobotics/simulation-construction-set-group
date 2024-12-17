b4c645a36f786e9f56994d215fb8e733b1d480f9
nkitchel <nkitchel@ihmc.org>
2024-12-17 17:19:57 -0600

    :arrow_up: ihmc-graphics-description 0.26.0

--- ihmc-robot-description/build.gradle.kts
+++ ihmc-robot-description/build.gradle.kts
@@ -12,7 +12,7 @@ ihmc {
 mainDependencies {
    api("us.ihmc:euclid:0.22.2")
 
-   api("us.ihmc:ihmc-graphics-description:0.21.1")
+   api("us.ihmc:ihmc-graphics-description:0.26.0")
 }
 
 testDependencies {
--- ihmc-swing-plotting/build.gradle.kts
+++ ihmc-swing-plotting/build.gradle.kts
@@ -17,11 +17,11 @@ mainDependencies {
    api("us.ihmc:euclid:0.22.2")
    api("us.ihmc:ihmc-commons:0.34.0")
 
-   api("us.ihmc:ihmc-graphics-description:0.21.1")
+   api("us.ihmc:ihmc-graphics-description:0.26.0")
 }
 
 testDependencies {
    api("us.ihmc:ihmc-commons-testing:0.34.0")
 
-   api("us.ihmc:ihmc-graphics-description-test:source")
+   api("us.ihmc:ihmc-graphics-description-test:0.26.0")
 }
