d6da074f0a023d6a192df5c6047ea390a8fdfbe9
nkitchel <nkitchel@ihmc.org>
2024-12-17 11:10:20 -0600

    Remove graphics-description, it lives in its own repository now

--- ihmc-graphics-description/src/main/java/us/ihmc/graphicsDescription/instructions/listeners/ScaleChangedListener.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.graphicsDescription.instructions.listeners;
-
-import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
-
-public interface ScaleChangedListener
-{
-   public void setScale(Vector3DReadOnly scale);
-}
--- ihmc-graphics-description/src/main/java/us/ihmc/graphicsDescription/plotting/artifact/ArtifactsChangedListener.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.graphicsDescription.plotting.artifact;
-
-import java.util.List;
-
-public interface ArtifactsChangedListener
-{
-   public void artifactsChanged(List<Artifact> newArtifacts);
-}
