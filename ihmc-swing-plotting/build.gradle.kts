7e51ccbb4a5effa73d1ca6c6461147527a4f94ef
Dexton Anderson <danderson@ihmc.org>
2025-02-05 11:59:55 -0600

    :arrow_up: ihmc-commons 0.35.1

--- ihmc-swing-plotting/build.gradle.kts
+++ ihmc-swing-plotting/build.gradle.kts
@@ -15,13 +15,13 @@ mainDependencies {
    api("org.jfree:jcommon:1.0.24")
 
    api("us.ihmc:euclid:0.22.2")
-   api("us.ihmc:ihmc-commons:0.34.0")
+   api("us.ihmc:ihmc-commons:0.35.1")
 
    api("us.ihmc:ihmc-graphics-description:0.26.0")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.34.0")
+   api("us.ihmc:ihmc-commons-testing:0.35.1")
 
    api("us.ihmc:ihmc-graphics-description-test:0.26.0")
 }
