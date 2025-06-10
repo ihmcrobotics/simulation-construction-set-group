de8ab0c4b8bbbe4807f2dfcfc2dc5f4f6220b0b6
Duncan Calvert <dcalvert@ihmc.org>
2025-06-10 17:14:34 -0500

    :arrow_up: scs2 17-0.30.0, logger 0.34.0

--- simulation-construction-set/build.gradle.kts
+++ simulation-construction-set/build.gradle.kts
@@ -51,7 +51,7 @@ mainDependencies {
    api("us.ihmc:euclid-shape:0.22.5")
    api("us.ihmc:euclid-geometry:0.22.5")
    api("us.ihmc:ihmc-commons:0.35.1")
-   api("us.ihmc:scs2-definition:17-0.29.0")
+   api("us.ihmc:scs2-definition:17-0.30.0")
 
    api("us.ihmc:ihmc-jmonkey-engine-toolkit:source")
    api("us.ihmc:ihmc-swing-plotting:source")
