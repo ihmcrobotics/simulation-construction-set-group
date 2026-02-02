b624a87a74eec299ff7b323d348bb39186da5661
Robert <rjgriffin42@gmail.com>
2026-02-02 17:25:39 -0600

    :arrow_up: ihmc-yovariables 0.13.7

--- simulation-construction-set/build.gradle.kts
+++ simulation-construction-set/build.gradle.kts
@@ -44,7 +44,7 @@ mainDependencies {
       exclude(group = "org.easytesting", module = "fest")
    }
 
-   api("us.ihmc:ihmc-yovariables:0.13.6")
+   api("us.ihmc:ihmc-yovariables:0.13.7")
    api("us.ihmc:ihmc-video-codecs:2.1.6")
    api("us.ihmc:euclid:0.22.5")
    api("us.ihmc:euclid-frame:0.22.5")
@@ -65,5 +65,5 @@ testDependencies {
 
 utilitiesDependencies {
    api("us.ihmc:euclid:0.22.5")
-   api("us.ihmc:ihmc-yovariables:0.13.6")
+   api("us.ihmc:ihmc-yovariables:0.13.7")
 }
